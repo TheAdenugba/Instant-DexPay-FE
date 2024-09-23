17bcd5aea86589e97295a7ca08df666fd2797b59
narthkings <imohdumto@gmail.com>
2024-09-23 16:38:09 +0100

    react query added with component recomposing

---
+++ utils/RectQueryClient.tsx
@@ -0,0 +1,13 @@
+'use client'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+
+const queryClient = new QueryClient()
+
+export const ReactQueryClientProvider = ({ children }: { children: React.ReactNode; }) => {
+    return (
+        // Provide the client to your App
+        <QueryClientProvider client={queryClient}>
+            {children}
+        </QueryClientProvider>
+    )
+};
